983116fbff86a4f0b25063fc950339fe68fa7b92
Nguyen Thanh Liem <nguyenthanhliem2112@gmail.com>
2024-09-28 22:19:00 +0700

    ok nha

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo "Hi mình là Thanh Liêm";
+    session_start();
+    if(!isset($_SESSION['giohang'])){
+        $_SESSION['giohang']=[];
+    }
+    if (isset($_GET['mod'])) {
+        switch ($_GET['mod']) {
+            case 'page':
+                include_once 'Controller/page.php';
+                break;
+            case 'user':
+                include_once 'Controller/user.php';
+                break;   
+            default:
+                header("Location: ?mod=page&act=trangchu");
+                break;  
+        }
+    } else {
+        header("Location: ?mod=page&act=trangchu");
+    }
+    ?>  
